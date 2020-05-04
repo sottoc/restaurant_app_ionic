2e4645e3d3ad74ee7f9c0afa1aa083d274c9ea1f
Tamara Milenkovic <baymax.development@gmail.com>
2020-05-04 04:11:27 +0200

    update server URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'http://ec2-3-95-2-78.compute-1.amazonaws.com:8000'
+  api_url: 'http://ec2-3-95-253-67.compute-1.amazonaws.com:8000'
 };
